9fa54283e4f567e245b9575530c8e84c5774e465
Ícaro Cortês <ibsc013@gmail.com>
2026-09-20 20:29:11 -0300

    fix: logs in slurm script

--- navier_stokes/slurm_scripts/amd-512.sh
+++ navier_stokes/slurm_scripts/amd-512.sh
@@ -2,15 +2,21 @@
 #SBATCH --time=0-1:0
 #SBATCH --partition=amd-512
 #SBATCH --cpus-per-task=128
-#SBATCH --output=logs/%x/%j.out.log
-#SBATCH --error=logs/%x/%j.err.log
+#SBATCH --output=/deb/null
+#SBATCH --error=/dev/null
 #SBATCH --exclusive
 
 # Script for NPAD cluster, npad.ufrn.br
+LOG_PATH="logs/$SLURM_JOB_NAME/"
+mkdir -p "$LOG_PATH"
+# log files for this script
+exec > "$LOG_PATH/master.out.log"
+exec 2> "$LOG_PATH/master.err.log"
 
 source ~/pascal-releases-master/env.sh
 
-srun pascalanalyzer ./wrapper_pascal.sh\
+srun --output="logs/%x/%j.out.log" --error="logs/%x/%j.err.log"\
+   pascalanalyzer ./wrapper_pascal.sh\
    --inst aut --idtm 5 --rpts 3\
    --cors 2,8,32,128\
    --ipts 500, 1000, 2000, 8000, 16000\
