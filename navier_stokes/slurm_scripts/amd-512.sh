b92b6c9e5f590f0ca986e100fbc6ccea2cf43a03
Ícaro Cortês <ibsc013@gmail.com>
2026-09-20 22:26:24 -0300

    fix: logs again

--- navier_stokes/slurm_scripts/amd-512.sh
+++ navier_stokes/slurm_scripts/amd-512.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-#SBATCH --time=0-1:0
+#SBATCH --time=0-12:0
 #SBATCH --partition=amd-512
 #SBATCH --cpus-per-task=128
-#SBATCH --output=/deb/null
+#SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 #SBATCH --exclusive
 
 # Script for NPAD cluster, npad.ufrn.br
-LOG_PATH="logs/$SLURM_JOB_NAME/"
+LOG_PATH="logs/$SLURM_JOB_NAME/$SLURM_JOB_ID"
 mkdir -p "$LOG_PATH"
 # log files for this script
 exec > "$LOG_PATH/master.out.log"
 exec 2> "$LOG_PATH/master.err.log"
 
-source ~/pascal-releases-master/env.sh
+pushd ~/pascal-releases-master
+source /env.sh
+popd
 
-srun --output="logs/%x/%j.out.log" --error="logs/%x/%j.err.log"\
+echo "Running at $SLURM_JOB_NODELIST."
+
+srun --output="$LOG_PATH/log.out" --error="$LOG_PATH/log.err"\
    pascalanalyzer ./wrapper_pascal.sh\
    --inst aut --idtm 5 --rpts 3\
    --cors 2,8,32,128\
-   --ipts 500, 1000, 2000, 8000, 16000\
+   --ipts 500,1000,2000,8000,16000\
    --verb INFO -o navier_stokes_pascal0.json
